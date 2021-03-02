475451900eb46ad6dfc789f8bc3405c0766dcb3d
Luis <tennis.bresil@gmail.com>
2021-03-02 09:17:11 -0500

    s
    s

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,21 +6,14 @@
         <p class="lead">Este é um simples componente jumbotron para chamar mais atenção a um determinado conteúdo ou
             informação.</p>
         <hr class="my-4">
-        <p>Restons en contact
-SIÈGE SOCIAL
-175 rue Louis-Hébert, Mascouche, QC J7K 0R5
-Canada
-
-POUR NOUS JOINDRE
-Courriel : info@animoetc.com
-Téléphone : (514) 312-6460
-
-OPPORTUNITÉ D'EMPLOI
-Vous avez la passion des animaux? L'équipe d'Animo etc est à la recherche de talents, tant au toilettage que sur le plancher. Joignez une équipe de passionnés et faites vous aussi partie de la grande famille Animo etc.
-
-OPPORTUNITÉ D'AFFAIRES
-Animo etc est un concept de franchise québécois, piloté par une équipe de professionnels de la franchise depuis plus de 20 ans. Si vous avez la fibre entrepreneuriale et désirez ouvrir votre propre magasin Animo etc n'hésitez pas à communiquer avec nous pour plus de détails.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Leia mais</a>
+<br><div dir="ltr" class="gmail_signature"><div dir="ltr"><table style="font-family:&quot;Times New Roman&quot;;font-size:12px;color:rgb(87,86,92);text-align:left" cellspacing="0">
+<tbody><tr><td><img src="https://solutions66.com/signature/66_png_200px.png" alt="Solutions 66"></td>
+<td style="padding-left:10px"><span style="font-size:16px;font-weight:bold;color:rgb(66,121,188)">JEAN-LOUIS TAMBURINI</span>
+<span style="margin:5px">Programmeur analyste sénior</span><br>
+<a href="tel:4503131166" style="color:rgb(87,86,92)" target="_blank">450.313.1166 </a>
+<span style="color:rgb(66,121,188);font-size:10px">|</span>
+<a href="jeanlouis@solutions66.com" style="color:rgb(87,86,92)" target="_blank">jeanlouis@solutions66.com</a>
+<span style="color:rgb(66,121,188);font-size:16px">|</span><a href="https://solutions66.com/" style="color:rgb(87,86,92)" target="_blank">solutions66.com</a><br>175 louis-hébert, Mascouche, Qc, J7K 0R5</td></tr></tbody></table></div></div>
     </div> -
 @endsection
 
